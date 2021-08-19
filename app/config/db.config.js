bb8c27ef60c9efeeb32f8f956571a28b9b1f51eb
eraflav <eraflav@gmail.com>
2021-08-19 00:23:54 -0500

    First commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: process.env.HOST_DB,
+  USER: process.env.USER_DB,
+  PASSWORD: process.env.PASS_DB,
+  DB: process.env.NAME_DB,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
